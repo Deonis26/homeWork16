82d600f37b0176523ba7426d16e7defb04652153
Denis <85250266+Deonis26@users.noreply.github.com>
2023-09-17 23:26:07 +0300

    изменил класс студент

--- src/src/by/corp/LDA/Main.java
+++ src/src/by/corp/LDA/Main.java
@@ -2,6 +2,13 @@ package by.corp.LDA;
 
 public class Main {
     public static void main(String[] args) {
-        Student pasha = new Student();
+        Student student1 = new Student("Артур", "Петров", 11, 111L, (short) 123);
+        Student student2 = new Student("Артур", "Петров", 1, 111L, (short) 123);
+        boolean a = student1.equals(student2);
+        System.out.println(a);
+        System.out.println(student2.hashCode());
+        String c=student2.toString();
+        System.out.println(student1.hashCode());
+        System.out.println(c);
     }
 }
--- src/src/by/corp/LDA/Student.java
+++ src/src/by/corp/LDA/Student.java
@@ -2,51 +2,61 @@ package by.corp.LDA;
 
 public class Student {
 
+    private String name;
+    private String sureName;
     private int age;
     private long id;
     private short academicPerformance;
     public Student() {
 
     }
-    public Student(int age, long id, short academicPerformance) {
+    public Student(String name, String sureName, int age, long id, short academicPerformance) {
+        this.name = name;
+        this.sureName=sureName;
         this.age = age;
         this.id = id;
         this.academicPerformance = academicPerformance;
     }
 
     public String toString() {
-        return " Возраст: " + age + ", Номер карты: " + id + "успеваемость: " + academicPerformance + ".";
+        return "Имя: "+name+", Фамилия: "+ sureName +"Возраст: " + age + ", Номер карты: " + id + "успеваемость: " + academicPerformance + ".";
     }
 
     @Override
     public int hashCode() {
-        return age + (int) id + (int) academicPerformance;
+        return name.hashCode()+sureName.hashCode()+ age*9 + (int) id + (int) academicPerformance;
     }
-
+    public String getName(){
+        return name;
+    }
+    public String getSureName(){
+        return sureName;
+    }
     public int getAge() {
         return age;
     }
 
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setSureName(String sureName){
+        this.sureName = sureName;
+    }
     public void setAge(int age) {
         this.age = age;
     }
-
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public short getAcademicPerformance() {
         return academicPerformance;
     }
-
     public void setAcademicPerformance(short academicPerformance) {
         this.academicPerformance = academicPerformance;
     }
-
     public boolean equals(Object object) {
         Student student;
         if (object instanceof Student) {
@@ -54,7 +64,8 @@ public class Student {
         } else {
             return false;
         }
-        if (age == (student.getAge()) && id == (student.getId()) &&
+        if (name.equals(student.getName()) && sureName.equals(student.getSureName()) &&
+                age == (student.getAge()) && id == (student.getId()) &&
                 academicPerformance == (student.getAcademicPerformance())) {
             return true;
         } else {
